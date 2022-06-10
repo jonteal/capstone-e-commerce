80f38e6e78f450329674108445cf63ef8c42d97a
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-06-10 16:23:19 -0500

    Install firebase and created basic sign in component. No auth created yet

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -12,8 +12,11 @@ const Navigation = () => {
                     <CrownLogo className="logo" />
                 </Link>
                 <div className="nav-links-container">
-                    <Link className="nav-link" to='shop'>
-                        Shop
+                    <Link className="nav-link" to='/shop'>
+                        SHOP
+                    </Link>
+                    <Link className="nav-link" to='/sign-in'>
+                        SIGN IN
                     </Link>
                 </div>
             </div>
