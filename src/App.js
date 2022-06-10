133ebbbad6c70eafc1dd88f0785a68caac7fd733
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-06-10 15:35:09 -0500

    Created Navigation bar with basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/Navigation';
 
 import Home from "./routes/home/Home";
+
+
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am the shop page</h1>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route exact path='/' element={<Home />}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   )
 };
