4a520afdb11eaccedc0518d048d0516cbd9c5694
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-06-13 11:28:06 -0500

    CartIcon and CartDropdown components created

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,9 +1,12 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+import CartIcon from "../../components/CartIcon/CartIcon";
+import CartDropdown from "../../components/CartDropdown/CartDropdown";
+
 import { UserContext } from "../../contexts/user.context";
 
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import './navigation.scss';
@@ -31,7 +34,9 @@ const Navigation = () => {
                                 SIGN IN
                             </Link>
                         )}
+                        <CartIcon />
                 </div>
+                <CartDropdown />
             </div>
             <Outlet />
         </Fragment>
