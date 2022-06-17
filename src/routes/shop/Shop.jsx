c129cfcd7d3781d20a3ef0e7f1f1c9cb15ef5ee8
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-06-17 16:05:11 -0500

    Categories moved over to redux. useContext not being used for this data now

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/CategoriesPreview';
 import Category from '../category/Category';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategoriesMap } from '../../store/categories/category.action';
 
 const Shop = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            // console.log(categoryMap);
+            dispatch(setCategoriesMap(categoryMap));
+        }
+        getCategoriesMap();
+    }, []);
+
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} />
