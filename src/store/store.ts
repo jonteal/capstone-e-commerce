bd3b58da8a97859ce6c1d305d4b9fc0318b69d15
Jon Jackson <jonjackson.webdesign@gmail.com>
2022-09-05 17:18:37 -0500

    fully typed redux. sagas next

---
+++ src/store/store.ts
@@ -0,0 +1,62 @@
+import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import logger from "redux-logger";
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from "@redux-saga/core";
+
+import { rootSaga } from "./root-saga";
+
+import { rootReducer } from "./root-reducer";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+    whitelist: (keyof RootState)[]
+}
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+const persistConfig: ExtendedPersistConfig = {
+  // key is the root, which means cover it all
+  key: "root",
+
+  // uses local storage by default
+  storage,
+
+  // tells the code to only persist items in the cart in by saving
+  // them in local storage
+  whitelist: ["cart"],
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
+].filter((middleware): middleware is Middleware => Boolean(middleware));
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
