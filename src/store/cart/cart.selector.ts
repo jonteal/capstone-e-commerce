8ded9939e582729793acaf60a1eddf8ae1e5421c
Jon Jackson <jonjackson.webdesign@gmail.com>
2022-09-05 16:55:03 -0500

    Typed the user fully

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -2,7 +2,7 @@ import { createSelector } from "reselect";
 
 import { CartState } from "./cart.reducer";
 
-const selectCartReducer = (state): CartState => state.cart;
+const selectCartReducer = (state: any): CartState => state.cart;
 
 export const selectIsCartOpen = createSelector(
   [selectCartReducer],
